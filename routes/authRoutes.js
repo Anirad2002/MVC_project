7a24d261911e03ace4041d4005ef423bbc023d54
Pasiura Daryna <107425596+Anirad2002@users.noreply.github.com>
2024-05-11 11:40:07 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,25 +1,3 @@
-/*// routes/authRoutes.js
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Маршрут для відображення форми реєстрації
-router.get('/register', authController.getRegisterForm);
-
-// Маршрут для обробки реєстрації користувача
-router.post('/register', authController.registerUser);
-
-// Маршрут для відображення форми авторизації
-router.get('/login', authController.getLoginForm);
-
-// Маршрут для обробки авторизації користувача
-router.post('/login', authController.loginUser);
-
-// Маршрут для виходу користувача з системи
-router.get('/logout', authController.logoutUser);
-
-module.exports = router;*/
-
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
@@ -37,7 +15,7 @@ router.get('/login', authController.getLoginForm);
 // Маршрут для обробки авторизації користувача
 router.post('/login', authController.loginUser);
 
-// Route for user logout
+// Маршрут для виходу користувача
 router.get('/logout', authController.logoutUser);
 
 module.exports = router;
