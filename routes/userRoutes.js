4466f201b67a15c68bfd2cf62d442a7c847ec2b0
Pasiura Daryna <107425596+Anirad2002@users.noreply.github.com>
2024-05-11 11:52:18 +0200

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,8 @@
+
 // routes/userRoutes.js
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
-// Маршрут для відображення форми реєстрації
-//router.get('/register', userController.getRegisterForm);
-
-// Маршрут для обробки реєстрації користувача
-//router.post('/register', userController.registerUser);
-
-// Маршрут для відображення форми авторизації
-//router.get('/login', userController.getLoginForm);
-
-// Маршрут для обробки авторизації користувача
-//router.post('/login', userController.loginUser);
 
 // Маршрут для відображення профілю користувача
 router.get('/profile', userController.getUserProfile);
@@ -21,7 +10,5 @@ router.get('/profile', userController.getUserProfile);
 // Маршрут для редагування інформації профілю користувача
 router.post('/profile/edit', userController.editUserProfile);
 
-// Маршрут для виходу користувача з системи
-router.get('/logout', userController.logoutUser);
 
 module.exports = router;
